3f3cb6e8fb5ae8e602b9d37da798f904e85381fa
fatimaasylbekkyzy <fatimaassylbekkyzy@gmail.com>
2026-06-02 15:58:19 +0800

    Refractor temperature converter program into separate functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -6,19 +6,37 @@ Program for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit : "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+
+def main():
+    """Program to convert Celsius to Fahrenheit and vise versa."""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_c_to_f(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            celsius = convert_f_to_c(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_f_to_c(fahrenheit):
+    """Convert fahrenheit to celsius."""
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def convert_c_to_f(celsius):
+    """Convert celsius to fahrenheit."""
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()
