d482c07da4aad2e499738746d8e4190b9fda47bf
fatimaasylbekkyzy <fatimaassylbekkyzy@gmail.com>
2026-06-02 19:26:21 +0800

    Refractor score status determiner program into separate functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,26 @@
 """
 CP1404/CP5632 - Practical
-Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    """Program to determine score status"""
+    score = float(input("Enter score: "))
+    status = determine_grade(score)
+    print(f"User score {score} is {status}.")
+
+
+def determine_grade(score):
+    """Determine the score status based on the score value"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
