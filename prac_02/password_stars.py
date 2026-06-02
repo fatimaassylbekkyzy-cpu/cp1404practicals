630553a12db82cf4e095b3279a5ea974e78ccf8b
fatimaasylbekkyzy <fatimaassylbekkyzy@gmail.com>
2026-06-02 10:25:42 +0800

    Refractor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,20 @@
 MIN_PASSWORD_LENGTH = 8
 
-password = input("Password: ")
-while len(password) < MIN_PASSWORD_LENGTH:
-	print("Password doesn't meet a minimum length of 8 characters.")
+def main():
+	password = get_password()
+	print_asterisks(password)
+
+
+def print_asterisks(password):
+	print('*' * len(password))
+
+
+def get_password():
 	password = input("Password: ")
-print('*' * len(password))
+	while len(password) < MIN_PASSWORD_LENGTH:
+		print("Password doesn't meet a minimum length of 8 characters.")
+		password = input("Password: ")
+	return password
+
+
+main()
