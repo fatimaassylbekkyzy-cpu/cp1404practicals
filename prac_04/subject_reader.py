4ffde8f77c850ff8be96f3acf71cc9f7d1217f80
fatimaasylbekkyzy <fatimaassylbekkyzy@gmail.com>
2026-06-16 20:26:02 +0800

    Complete subject data loading and formatting program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,24 +8,34 @@ FILENAME = "subject_data.txt"
 
 def main():
     """Program to load and display subject data from file."""
-    data = load_data(FILENAME)
-    print(data)
+    subject_data = load_subjects(FILENAME)
+    display_subjects(subject_data)
 
 
-def load_data(filename=FILENAME):
+def display_subjects(subject_data):
+    """Display subject data a different way."""
+    for parts in subject_data:
+        subject_code = parts[0]
+        lecturer = parts[1]
+        number_of_students = parts[2]
+        print(f"{subject_code} is taught by {lecturer:12} and has {number_of_students:3} students")
+
+
+def load_subjects(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(filename)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        # Make the number an integer as part of a new, poorly named, list
-        data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
-        print("----------")
-    input_file.close()
+    subject = []
+    with open(filename) as input_file:
+        for line in input_file:
+            # print(line)  # See what a line looks like
+            # print(repr(line))  # See what a line really looks like
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            parts[2] = int(parts[2])
+            # print(parts)  # See what the parts look like (notice the integer is a string)
+            # data = [parts[0], parts[1], int(parts[2])]
+            # print(data)  # See if that worked
+            subject.append(parts)
+    return subject
 
 
 main()
